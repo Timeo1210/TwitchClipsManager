d5ac1139ab062adb678cdec2a01fa809441c0186
Timeo1210 <timeo.boulhol@gmail.com>
2021-07-13 01:01:19 +0200

    refactor(server): remove dev stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,7 @@ import "reflect-metadata";
 import { initAdapters } from "@/infrastructure/adapters/init";
 import { initControllers } from "@/interface-adapters/controllers/init";
 
-import { ChannelService } from "@/core/services/channel.service";
-
 (async () => {
   await initAdapters.init();
   await initControllers.init();
-  const str = "Sardoche";
-
-  const data = await ChannelService.search({ query: str });
-  console.log(data);
 })();
