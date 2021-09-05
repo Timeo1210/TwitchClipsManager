7189a13ef5708efaa75892bf3d3af2622bc6332f
Timeo1210 <timeo.boulhol@gmail.com>
2021-09-05 16:15:16 +0200

    feat(server): Add pagination field to Video resolver Graphql

---
+++ server/src/interface-adapters/controllers/apollo/ObjectTypes/Videos.objectType.ts
@@ -0,0 +1,18 @@
+/* eslint-disable max-classes-per-file */
+import { Field, ObjectType } from "type-graphql";
+import { Video } from "./Video.objectType";
+
+@ObjectType()
+class Pagination {
+  @Field()
+  cursor: String;
+}
+
+@ObjectType()
+export class Videos {
+  @Field(() => [Video])
+  videos: Video[];
+
+  @Field(() => Pagination)
+  pagination: Pagination;
+}
--- server/src/interface-adapters/controllers/apollo/resolvers/Video.resolver.ts
+++ server/src/interface-adapters/controllers/apollo/resolvers/Video.resolver.ts
@@ -1,18 +1,23 @@
+/* eslint-disable max-classes-per-file */
 import { Args, Query, Resolver } from "type-graphql";
 import { UserInputError } from "apollo-server-core";
 
 import { VideoService } from "@/core/services/video.service";
 
-import { Video } from "../ObjectTypes/Video.objectType";
+import { Videos } from "../ObjectTypes/Videos.objectType";
 import { GetByUserVideoArgs } from "../ArgsTypes/Video.argsType";
 
-@Resolver(Video)
+@Resolver(Videos)
 export class VideoResolver {
-  @Query(() => [Video])
+  @Query(() => Videos)
   async getByUser(@Args() request: GetByUserVideoArgs) {
     try {
       const response = await VideoService.getByUser(request);
-      if (response) return response.data;
+      if (response)
+        return {
+          videos: response.data,
+          pagination: response.pagination,
+        };
     } catch {
       throw new Error("Internal Server Error !");
     }
