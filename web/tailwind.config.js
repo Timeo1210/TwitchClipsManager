ea02131c138964d6542376c27964f7ebf91b24ea
Timeo1210 <timeo.boulhol@gmail.com>
2021-10-09 02:50:33 +0200

    feat(web): add line clamp and custom tailwind plugin

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -10,5 +14,16 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    plugin(({ addUtilities }) => {
+      const newResponsiveUtilities = {
+        ".items-normal": {
+          alignItems: "normal",
+        },
+      };
+
+      addUtilities(newResponsiveUtilities, ["responsive"]);
+    }),
+  ],
 };
