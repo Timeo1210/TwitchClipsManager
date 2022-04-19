6b03ec11691437c91a317b1e8d4e9261d048f7bc
Timeo1210 <timeo.boulhol@gmail.com>
2022-04-19 19:11:10 +0200

    fix(server): Refreshing Twitch Setup timeout was too big

--- server/src/infrastructure/adapters/twitchapi/config.ts
+++ server/src/infrastructure/adapters/twitchapi/config.ts
@@ -17,7 +17,10 @@ export const TwitchApiAdapter = {
     secret: process.env.TWITCHAPI_SECRET || "",
     gql_id: process.env.TWITCHAPI_GQL_CLIENT || "",
   }),
-  refreshSetup: async (): Promise<number> => TwitchApiAdapter.API.setup(),
+  refreshSetup: async (): Promise<void> => {
+    console.log("Refreshing Twitch Setup...");
+    TwitchApiAdapter.API.setup();
+  },
   run: async (): Promise<void> => {
     if (process.env.TWITCHAPI_ENABLE === "true") {
       await TwitchApiAdapter.refreshSetup();
@@ -25,7 +28,7 @@ export const TwitchApiAdapter = {
     // const nextSetupTime = 250;
     setTimeout(
       TwitchApiAdapter.refreshSetup,
-      86400 * 1000 // 1 day
+      1.8e6 // 30 min
     );
   },
   Validators: {
