43d56003186144b4cfa78e1cc2b6c333800a869f
Kali Live user <timeo.boulhol@gmail.com>
2022-06-01 12:55:54 +0000

    fix(server): sqlite fix invalid type in videostate controller

--- server/src/infrastructure/repositories/sqlite/controllers/videostate.controller.ts
+++ server/src/infrastructure/repositories/sqlite/controllers/videostate.controller.ts
@@ -1,25 +1,26 @@
 import {
   getVideostateByIdRequest,
-  getVideostateByIdResponse,
+  // getVideostateByIdResponse,
   postVideostateRequest,
   postVideostateResponse,
 } from "@/infrastructure/repositories/interfaces/videostate/videostate.controller.interface";
-import { VideostateEntity } from "@/core/entity/Videostate.entity";
+// import { VideostateEntity } from "@/core/entity/Videostate.entity";
 import { db } from "../config";
 
-const getVideostateById = (
-  request: getVideostateByIdRequest
-): getVideostateByIdResponse => {
-  try {
-    const videostateQuery: VideostateEntity | undefined = db
-      .prepare(`SELECT id,state FROM videos WHERE id=?`)
-      .get(request.id);
+// const getVideostateById = (
+//   request: getVideostateByIdRequest
+// ): getVideostateByIdResponse => {
+//   try {
+//     const videostateQuery: VideostateEntity | undefined = db
+//       .prepare(`SELECT id,state FROM videos WHERE id=?`)
+//       .get(request.id);
 
-    return videostateQuery;
-  } catch {
-    return undefined;
-  }
-};
+//     return videostateQuery;
+//   } catch {
+//     return undefined;
+//   }
+// };
+const getVideostateById = (_: getVideostateByIdRequest): undefined => undefined;
 
 const postVideostate = (
   request: postVideostateRequest
